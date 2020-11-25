7710c9af54db37ee5bb746c634b30e1b738e3464
manhnamng <mn.manhnam@gmail.com>
2020-11-25 18:22:01 +0700

    Create demo1.cpp

---
+++ demo1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	// cac lenh chuong trinh
+	int a;
+	printf("Nhap a= ");	
+	scanf("%d",&a);
+	
+	int b;
+	printf("Nhap b= ");
+	scanf("%d",&b);
+	
+	int c;
+	c = a + b;
+	
+ 	printf("Display C = %d",c);
+ 	
+}
